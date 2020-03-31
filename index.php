7d6e5242bc6f0e4100c68572a30513c83f26e8b1
GWS-CODEFE <gws-codefe@GWS-CODEFEs-MacBook-Air.local>
2020-03-31 12:26:35 +0530

    tracking code uploaded

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 <html class="no-js" lang="en-US">
    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
    <head>
+
+      <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WZLPRNC');</script>
+
       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Online Drugstore | Epic Pharmacy</title>
@@ -62,6 +70,12 @@
       <!-- endinject -->
    </head>
    <body class="home page-template-default page page-id-6" data-load="false" data-menu="false" data-reservation="false" data-scrolled="false">
+
+      <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZLPRNC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
       <?php
          include("header-home.php");
          ?>
