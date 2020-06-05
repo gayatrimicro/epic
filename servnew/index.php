d2953d4478a0a64b57ef24e72994e64597f3c2d5
prashantgmicro <pgole31@gmail.com>
2020-06-05 09:59:58 +0530

    chn

---
+++ servnew/index.php
@@ -0,0 +1,429 @@
+
+<!DOCTYPE html> 
+<html class="no-js" lang="en-US"> 
+
+<head>
+	
+	<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WZLPRNC');</script>
+<!-- End Google Tag Manager -->
+	
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+
+<meta name="google-site-verification" content="A-llZMVa8hZh-UpSwLh1pVOmWUxxhd8MrBf7OTQjtEE" />
+ 
+  <title>Medical Store Services | Epic Pharmacy</title>
+
+  <meta name="description" content="Chemist Home Delivery Services. Call 888-688-0054 toll-free for specialty prescription drugs." >
+
+  <meta name="keywords" content="instant online medicine delivery near me, online medical shop, online drugstore, epic pharmacy drugstore in oklahoma, medical services">
+
+
+  <meta property="og:locale" content="en_US">
+  <meta name="og:site_name" content="Epic Pharmacy"/>
+  <meta property="og:type" content="Website">
+  <meta property="og:title" content="Medical Store Services | Epic Pharmacy">
+  <meta property="og:description" content="Chemist Home Delivery Services. Call 888-688-0054 toll-free for specialty prescription drugs.">
+  <meta property="og:image" content="https://epicpharmacyokc.com/assets/images/center-mobile.jpg">
+  <meta property="og:url" content="https://epicpharmacyokc.com/services/">
+
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:title" content="Medical Store Services | Epic Pharmacy">
+  <meta name="twitter:description" content="Chemist Home Delivery Services. Call 888-688-0054 toll-free for specialty prescription drugs.">
+  <meta name="twitter:image" content="https://epicpharmacyokc.com/assets/images/center-mobile.jpg">
+  <meta name="twitter:url" content="https://epicpharmacyokc.com/services/">
+
+  <meta name="robots" content="index, follow" />
+
+  <link rel="canonical" href="https://epicpharmacyokc.com/services/">
+  <link rel="alternate" href="https://epicpharmacyokc.com/services/" hreflang="en-us" />
+
+  <link rel="apple-touch-icon" href="../wp-content/themes/epic-assets/static/favicon/Epic-Logo.png">
+
+  <link rel="icon" type="image/png" sizes="32x32" href="../wp-content/themes/epic-assets/static/favicon/Epic-Logo.png">
+
+  <link rel="icon" type="image/png" sizes="16x16" href="../wp-content/themes/epic-assets/static/favicon/Epic-Logo.png">
+
+  <link rel="manifest" href="../wp-content/themes/epic-assets/static/favicon/Epic-Logo.png">
+
+  <link rel="mask-icon" href="../wp-content/themes/epic-assets/static/favicon/Epic-Logo.png" color="#455d58">
+
+  <link rel="shortcut icon" href="../wp-content/themes/epic-assets/static/favicon/Epic-Logo.png">
+
+  
+
+<!-- / Yoast SEO plugin. -->
+
+<link rel='dns-prefetch' href='//s.w.org' />
+		
+
+
+		<link rel='stylesheet' id='wp-block-library-css'  href='../wp-includes/css/dist/block-library/style.min6619.css?ver=5.2.5' type='text/css' media='all' />
+
+
+<link rel='https://api.w.org/' href='../wp-json/index.html' />
+
+
+<link rel='shortlink' href='../indexd8eb.html?p=17' />
+
+
+<link rel="alternate" type="application/json+oembed" href="../wp-json/oembed/1.0/embedaa3d.json?url=https%3A%2F%2Fwww.javascript:void(0);%2Fmeetings-events%2F" />
+
+
+<link rel="alternate" type="text/xml+oembed" href="../wp-json/oembed/1.0/embedd604?url=https%3A%2F%2Fwww.javascript:void(0);%2Fmeetings-events%2F&amp;format=xml" />
+  <!-- inject:css -->
+<!-- <link rel="stylesheet" href="../wp-content/themes/epic-assets/static/css/style-5997a7ee39.css"> -->
+<link rel="stylesheet" href="../wp-content/themes/epic-assets/static/css/style-821bd475c3.css">
+<link rel="stylesheet" type="text/css" href="../assets/css/newstyle.css">
+<!-- prabhat pandit stylesheets starts from here.. on 17/02/2020 !-->
+<style type="text/css">
+  .custom-site-header {
+    border: 0;
+    -ms-interpolation-mode: bicubic;
+    vertical-align: middle;
+    max-width: none;
+    height: auto;
+    width: 202px !important;
+    height: auto;
+}
+
+.custom-site-header {
+    border: 0;
+    -ms-interpolation-mode: bicubic;
+    vertical-align: middle;
+    max-width: none;
+    height: auto;
+    margin-top: 6px;
+    width: 202px !important;
+    height: auto;
+}
+.caption {
+    color: #48a748 !important;
+}
+.site-header__reservations-list svg {
+    display: inline-block;
+    fill: #47a748 !important;
+    height: 1.6rem;
+    transition: fill .3s;
+    transition-delay: 0s;
+    width: 1.9rem;
+}
+.site-menu__block-colour {
+    background-color: #002b46;
+}
+.t7{
+  width: 100%;
+}
+@media only screen and (max-width: 600px){
+.custom-site-header {
+    border: 0;
+    -ms-interpolation-mode: bicubic;
+    vertical-align: middle;
+    max-width: none;
+    height: auto;
+    width: 120px !important;
+    height: auto;
+}
+.sister-panels{
+  display: none;
+}
+}
+</style>
+	
+	<!--json schema -->
+	<script type="application/ld+json">
+		{
+		  "@context": "http://schema.org",
+		  "@type": "Store",
+		  "name": "Oklahoma's online Epic Pharmacy",
+		  "description": "Contact Oklahoma's online Epic Pharmacy to refill prescriptions. Find internet pharmacy for delivery near me",
+		  "openingHours": "Mo-Fr 09:00am - 06:00pm Sat 09:00am - 06:00pm",
+		  "telephone": "888-688-0054",
+		  "image": "https://epicpharmacyokc.com/assets/images/center-mobile.jpg",
+		  "priceRange": "$0",
+		  "address":
+		  {
+		  "@type": "PostalAddress",
+		  "streetAddress": "2249 NW 39th Street",
+		  "addressLocality": "Oklahoma City",
+		  "addressRegion": "OK",
+		  "postalCode": "73112",
+		  "addressCountry": "USA"
+		  },
+		  "department":
+		  {
+			"@type": "Pharmacy",
+			"name": "Epic Pharmacy",
+			"description": "Leading suppliers of pharmacy services and specialty medications. Buy medicines online with a discount on prescription drugs",
+			"openingHours": "Mo-Fr 09:00am -06:00pm Sat 09:00am -06:00pm",
+			"telephone": "888-688-0054",
+			"image": "https://epicpharmacyokc.com/assets/images/center-mobile.jpg",
+			"priceRange": "$0",
+			"address":
+			{
+			  "@type": "PostalAddress",
+			  "streetAddress": "2249 NW 39th Street",
+			  "addressLocality": "Oklahoma City",
+			  "addressRegion": "OK",
+			  "postalCode": "73112",
+			  "addressCountry": "USA"
+			}
+		  }
+		}
+	</script>
+	<!--json schema -->
+	
+</head>
+
+
+
+<body class="page-template-default page page-id-33" data-load="false" data-menu="false" data-reservation="false" data-scrolled="false">
+  
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZLPRNC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<?php
+include("../header-base.php");
+?>
+
+
+
+
+<div class="js-ajax-container" data-namespace="page">
+
+  <main class="page-content">
+
+  
+<section class="SecExp">
+  <div class="container">
+    <h2>PRE-EXPOSURE <br>PROPHYLAXIS (PREP)</h2>
+    <p>At ASP Cares, we understand the importance of preventing and transmitting HIV. Fortunately, PrEP was approved in 2012 and stands for Pre-Exposure Prophylaxis. It means that a medication can be taken for this purpose. PEP stands for Post-ExPosure. It means that a medication regimen can be taken if you have been exposed to HIV.</p>
+    <p>Our pharmacists and nurses are dedicated to helping patients understand prophylaxis and how to access this therapy from a physician. We will provide you with education, help you find a provider and process your prescription through your insurance plan. It’s a simple process and starts with a call to us (1-800-950-2919) and then a visit to a physician who has expertise with PrEP.</p>
+    <p>Consistently since 2012, ASP Cares has been highly recommended by physicians, including Infectious Disease Specialists because of our sincere focus on patients. It’s true, we care about patients, but we are also committed to community advocacy and educating the public about the prevention of HIV.</p>
+    <hr>
+    <div class="row_col">
+      <div class="col_4">
+        <h4>Medication Access</h4>
+      </div>
+      <div class="col_8">
+        <p>ASP Cares provides patients with their medication and drug information. It is shipped, without cost, directly to the patient or another route of delivery or pick-up can be coordinated. Our healthcare team is committed to providing the best service, which is both confidential and individualized based on a patients’ specific needs. Whether you will be taking your medication for a short period of time or continuously, we recognize the importance of starting your medication when needed.</p>
+      </div>
+    </div>
+    <hr>
+    <div class="row_col">
+      <div class="col_4">
+        <h4>Insurance & Billing</h4>
+      </div>
+      <div class="col_8">
+        <p>Our reimbursement specialists handle coordination of benefits and are attentive to real life issues and the cost of PrEP. We gladly assist in prior authorization approval and accept the majority of insurance plans. Because cost could be a concern for some patients, our team is your advocate. We work diligently to find other financial assistance if needed.</p>
+      </div>
+    </div>
+    <hr>
+    <div class="row_col">
+      <div class="col_4">
+        <h4>Medication Delivery</h4>
+      </div>
+      <div class="col_8">
+        <p>ASP Cares ensures that you receive your medication timely by providing complimentary delivery directly to your home. We can also coordinate other medications as well.</p>
+      </div>
+    </div>
+    <hr>
+    <div class="row_col">
+      <div class="col_4">
+        <h4>Support</h4>
+      </div>
+      <div class="col_8">
+        <p>At ASP Cares, taking this step to start medication for PrEP may create many questions. Our team is here to support you and find the answers you need. Though not intended to replace medical advice from a qualified physician, resource links can provide additional information and support.</p>
+      </div>
+    </div>
+
+  </div>  
+</section>
+<section class="SecExpFix">
+  <div class="container">
+    <div class="in_imgFix">      
+      <div class="minSec">
+        <h3>ONLINE SUPPORT LINKS</h3>
+         <a href="" target="_blank">
+            <p>Center For Disease Control (CDC)/HIV Information</p>
+         </a>
+         <a href="" target="_blank">
+            <p>HIV Resources by State</p>
+         </a>
+         <a href="" target="_blank">
+            <p>Center for Medicaid and Medicare/Aids Information</p>
+         </a>
+         <a href="" target="_blank">
+         <p>Government/HIV Information</p>
+         </a>
+         <a href="" target="_blank">
+         <p>PrEP Information</p>
+         </a>
+         <a href="" target="_blank">
+         <p>American Academy of HIV Medicine</p>
+         </a>
+         <a href="" target="_blank">
+         <p>HIV Medicine Association</p>
+         </a>
+         <a href="" target="_blank">
+         <p>Aids Drug Assistance Program</p>
+         </a>
+         <a href="" target="_blank">
+         <p>Aids Information</p>
+         </a>
+         <a href="" target="_blank"><p>Truvada </p>
+         </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+  </main>
+
+          <div class="footer-el-wrap" data-anim="false">
+
+          
+          <!-- footer starts here !-->
+             <?php include '../footer.php';?>
+          <!-- footer ends here !-->
+        </div>
+
+        <div class="sister-panels" data-anim="false" data-anim-offset="0">
+          <div class="sister-hotel">
+
+  <div class="container">
+
+    <div class="cols">
+
+      <div class="col sister-hotel__col m12">
+
+        <div class="sister-hotel__inner">
+           <h3>Our Accreditations</h3>
+
+          <ul class="sister-hotel__list">
+            <li class="sister-hotel__list-item">
+              <a href="javascript:void(0);">
+                <img src="../wp-content/themes/epic-assets/static/img/sister-logos/clients-logo.png" width="123" height="51" alt="Accreditations" />
+              </a>
+            </li>
+            <li class="sister-hotel__list-item">
+              <a href="javascript:void(0);">
+                <img src="../wp-content/themes/epic-assets/static/img/sister-logos/cl.png" width="122" height="13" alt="Accreditations" />
+              </a>
+            </li>
+            <li class="sister-hotel__list-item">
+              <a href="javascript:void(0);">
+                <img src="../wp-content/themes/epic-assets/static/img/sister-logos/cl-1.png" width="119" height="28" alt="Accreditations" />
+              </a>
+            </li>
+            <li class="sister-hotel__list-item">
+              <a href="javascript:void(0);">
+                <img src="../wp-content/themes/epic-assets/static/img/sister-logos/cl-2.png" width="58" height="52" alt="Accreditations" />
+              </a>
+            </li>
+            <li class="sister-hotel__list-item">
+              <a href="javascript:void(0);">
+                <img src="../wp-content/themes/epic-assets/static/img/sister-logos/cl-3.png" width="90" height="44" alt="Accreditations" />
+              </a>
+            </li>
+          </ul>   
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>          
+<div class="sister-hotel">
+
+  <div class="container">
+
+    <div class="cols">
+
+      <div class="col sister-hotel__col m12">
+
+        <div class="sister-hotel__inner">
+
+          
+
+          <a href='javascript:void(0);' role='link' aria-label='' class='button js-block' data-block='button-block'  target='_self' ><span><span>E</span><span>x</span><span>p</span><span>l</span><span>o</span><span>r</span><span>e</span><span> </span><span>c</span><span>l</span><span>a</span><span>s</span><span>s</span><span>i</span><span>c</span><span> </span><span>h</span><span>o</span><span>t</span><span>e</span><span>l</span><span>s</span><span> </span><span>&</span><span> </span><span>r</span><span>e</span><span>s</span><span>o</span><span>r</span><span>t</span><span>s</span></span><span><span>E</span><span>x</span><span>p</span><span>l</span><span>o</span><span>r</span><span>e</span><span> </span><span>c</span><span>l</span><span>a</span><span>s</span><span>s</span><span>i</span><span>c</span><span> </span><span>h</span><span>o</span><span>t</span><span>e</span><span>l</span><span>s</span><span> </span><span>&</span><span> </span><span>r</span><span>e</span><span>s</span><span>o</span><span>r</span><span>t</span><span>s</span></span></a>        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>        </div>
+
+      </div> <!-- site-wrap -->
+    </div> <!-- scroll__runner -->
+  </div> <!-- scroll -->
+
+  
+  
+<script src="../wp-content/themes/epic-assets/static/dist/app-b163a97c46.js" async></script>
+<script src="../wp-content/themes/epic-assets/static/dist/gpvVDj0ljQ.js"></script>
+
+
+  
+<script>
+/* Please do not modify the below code. */
+a = WsVars.RateCode.toString();
+if(/[ABCD]/.test(a[1]))
+(function () {
+var params = {
+hpid: WsVars.HotelID, 
+pt: WsVars.Step, /* Page Type - CONVERSION or SEARCH or SHOPPING_CART */
+hd1: WsVars.YYYYMMDD1, /* Check In Date. Format yyyy-mm-dd. Ex: 2015-02-14 */
+hd2: WsVars.YYYYMMDD2, /* Check Out Date. Format yyyy-mm-dd. Ex: 2015-02-14 */
+hc1: "", /* Destination City */
+hs1: "", /* Destination State or Region */
+hpr: WsVars.HotelName, /* Hotel Property */
+hr: WsVars.Rooms, /* Number of Rooms */
+hc: WsVars.RoomType, /* Room type */
+t: parseInt(WsVars.Adults) + parseInt(WsVars.Child1) + parseInt(WsVars.Child2), /* Number of
+Travelers */
+hd: WsVars.Nights, /* Length of Travel in Nights */
+hp: WsVars.Amount, /* Purchase Price */
+hcu: WsVars.Currency, /* Purchase Currency */
+hdc: WsVars.RateCode, /* Rate Code */
+hconfno: WsVars.ResID, /* Confirmation Number */
+l: WsVars.LangCode /* Browser Language */
+};
+try{
+params.hc1 = WsVars.GeoAddr.split(",")[0];
+params.hs1 = WsVars.GeoAddr.split(",")[1];
+}catch(e){}
+switch(params.pt){
+case "2": params.pt = "SEARCH";break;
+case "4": params.pt = "SHOPPING_CART";break;
+case "5": params.pt = "CONVERSION";break;
+}
+var paramsArr = [];
+for(key in params) { paramsArr.push(key + '=' + encodeURIComponent(params[key])) };
+var pl = document.createElement('script');
+pl.type = 'text/javascript';
+pl.async = true;
+pl.src = "https://beacon.sojern.com/pixel/cp/21?f_v=cp_v1_js&p_v=1&" + paramsArr.join('&');
+(document.getElementsByTagName('head')[0] ||
+document.getElementsByTagName('body')[0]).appendChild(pl);
+})();
+</script>
+
+</body>
+</html>
+</div>
